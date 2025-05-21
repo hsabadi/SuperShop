30cfcd9a036845e9de4225dc815a8f5ed7022b3c
hsabadi <uuhsoabadi@gmail.com>
2025-05-21 22:42:27 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/xx/">
+  <BrowserRouter basename="/SuperShop/">
     <App />
   </BrowserRouter>
 );
