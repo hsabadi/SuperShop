b8ddc78c2078a24b17bc7536811a2a38ca66b676
hsabadi <uuhsoabadi@gmail.com>
2025-05-21 23:48:16 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/SuperShop">
-    <App />
-  </BrowserRouter>
-);
+<HashRouter basename="/SuperShop/">
+  <App />
+</HashRouter>
