3e562a1704a16845bb1145abcdafc042220e2b53
hsabadi <uuhsoabadi@gmail.com>
2025-05-21 22:42:07 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/xx/">
+  <BrowserRouter basename="/SuperShop/">
     <App />
   </BrowserRouter>
 );
